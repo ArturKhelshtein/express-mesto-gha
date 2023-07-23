6186e1b7d49f42b85284d94a9376f07803cc3a72
Artur Khelshtein <Khelshtein.a@yandex.ru>
2023-07-23 23:44:23 +0300

    fix: отработка тестов

--- middlewares/errorMiddleware.js
+++ middlewares/errorMiddleware.js
@@ -1,29 +1,31 @@
 const ErrorBadRequest = require('../errors/error-bad-request');
 const ErrorConflictRequest = require('../errors/error-conflict-request');
+const ErrorForbidden = require('../errors/error-forbidden');
 const ErrorNotFound = require('../errors/error-not-found');
 const ErrorUnauthorized = require('../errors/error-unauthorized');
+
+function returnError({ res, error }) {
+  return res
+    .status(error.statusCode)
+    .send({ message: error.message });
+}
 
 // eslint-disable-next-line consistent-return
 const errorMiddleware = (error, _req, res, next) => {
   if (error instanceof ErrorBadRequest) {
-    return res
-      .status(error.statusCode)
-      .send({ message: error.message });
+    returnError({ res, error });
   }
   if (error instanceof ErrorUnauthorized) {
-    return res
-      .status(error.statusCode)
-      .send({ message: error.message });
+    returnError({ res, error });
+  }
+  if (error instanceof ErrorForbidden) {
+    returnError({ res, error });
   }
   if (error instanceof ErrorNotFound) {
-    return res
-      .status(error.statusCode)
-      .send({ message: error.message });
+    returnError({ res, error });
   }
   if (error instanceof ErrorConflictRequest) {
-    return res
-      .status(error.statusCode)
-      .send({ message: error.message });
+    returnError({ res, error });
   }
 
   next();
--- routes/signup.js
+++ routes/signup.js
@@ -5,7 +5,7 @@ const { createUser } = require('../controllers/users');
 
 router.post('/', celebrate({
   body: Joi.object().keys({
-    email: Joi.string().required().min(2).max(30)
+    email: Joi.string().required()
       .pattern(/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/),
     password: Joi.string().required().min(2),
     name: Joi.string().min(2).max(30),
