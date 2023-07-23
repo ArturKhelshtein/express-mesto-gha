f52f9a7b6ebb2234908da55ba56affe48e92b61f
Artur Khelshtein <Khelshtein.a@yandex.ru>
2023-07-23 17:14:38 +0300

    fix: поправлено регулярное выражение avatar

--- routes/signup.js
+++ routes/signup.js
@@ -10,7 +10,7 @@ router.post('/', celebrate({
     password: Joi.string().required().min(2),
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string().pattern(/^https?:\/\/w*.?[a-zA-Z0-9-.+()[]_~:\/?#@!$&'*,;=]+/),
+    avatar: Joi.string().pattern(/^https?:\/\/[www.]?[-a-zA-Z0-9+&@/?=~_!:,.;()*'$\][]*#?/),
   }),
 }), createUser);
 
