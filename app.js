57c50f34d76c6dcbfeb8ae743de6c0906a8c40dd
Artur Khelshtein <Khelshtein.a@yandex.ru>
2023-07-24 16:14:57 +0300

    fix: lint

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const { errors } = require('celebrate');
 
 const router = require('./routes');
 const { errorMiddleware } = require('./middlewares/errorMiddleware');
-const { NOT_FOUND } = require('./utils/status-code');
 
 const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
@@ -23,12 +22,6 @@ app.use(router);
 
 app.use(errors());
 app.use(errorMiddleware);
-
-// app.use((req, res) => {
-//   res
-//     .status(NOT_FOUND)
-//     .send({ message: `Ресурс по адресу ${req.path} не найден` });
-// });
 
 // eslint-disable-next-line no-console
 app.listen(PORT, () => console.log(`Application is running on port ${PORT}`));
